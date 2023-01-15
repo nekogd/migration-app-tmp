9ca2e80aa3ac13a92bdc42939facaeba85463380
Łuczko Igor <tymekluczko@gmail.com>
2023-01-15 12:29:19 +0100

    first commit that should deploy

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
-const port = 5005;
+require('dotenv').config()
+
+const port = process.env.PORT || 5005;
 
 // set security HTTP headers
 app.use(helmet());
@@ -18,7 +20,7 @@ app.use(cors());
 app.options("*", cors());
 
 app.use(function (req, res, next) {
-  if (!req.headers.authorization) {
+  if (req.headers.authorization !== process.env.AUTHORIZATION_TOKEN) {
     return res.status(403).json({ error: "No credentials sent!" });
   }
   next();
@@ -27,11 +29,11 @@ app.use(function (req, res, next) {
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: "ofs_db_prod",
-  username: "bq_bi",
-  password: "wdxRDjWh9ZXwSdnmhqzU2wx3FLVtFQQ7",
-  host: "rpm-postgres.postgres.database.azure.com",
-  port: 5432,
+  database: process.env.OFS_DATABASE_NAME,
+  username: process.env.OFS_DATABASE_USERNAME,
+  password: process.env.OFS_DATABASE_PASSWORD,
+  host: process.env.OFS_DATABASE_HOST,
+  port: process.env.OFS_DATABASE_PORT,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
@@ -42,7 +44,7 @@ const sequelize = new Sequelize({
   },
 });
 
-const findUserByEmail = async (email) => {
+const OFSFindUserByEmail = async (email) => {
   try {
     //const email = 'v.valikova@bioniq.com'
     await sequelize.authenticate();
@@ -56,11 +58,10 @@ const findUserByEmail = async (email) => {
   }
 };
 
-app.post("/users/:email", async (req, res) => {
+app.post("/users/search-by-email", async (req, res) => {
   try {
     const email = req.body;
-
-    const user = await findUserByEmail(email);
+    const user = await OFSFindUserByEmail(email);
     console.log(user);
     res.status(200).json({ user });
   } catch (e) {
